37cda90616082cb7d56088803b50a633ee7fabce
Monica Grandy <monica.grandy@gmail.com>
2016-05-15 21:04:06 -0700

    adds api call and config to gitignore

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 "use strict";
-let koa = require('koa');  
+let koa = require('koa'); 
+let router = require('koa-router'); 
 let app = koa();
 let co = require("co");
 let request = require("co-request");
+let config = require("config")
+let credentials = config.get('credentials')
 
+//app.use(router.routes());
+let weatherData;
 app.use(function *() {  
-  this.body = 'Hello World';
+  this.body = weatherData || "Hello World";
 });
 
 co(function* () {
-  let result = yield request("http://api.openweathermap.org/data/2.5/weather?q=LosAngeles&appid=128c543da50fbcc0d216056029148b7e"); 
+  let result = yield request("http://api.openweathermap.org/data/2.5/weather?q=LosAngeles&appid=" + credentials.key); 
   let response = result;
-  let body = result.body;
-  console.log("Response: ", response);
-  console.log("Body: ", body);
+  let weatherData = result.body;
+  console.log("Body: ", weatherData)
 }).catch(function (err) {
     console.err(err);
 });
